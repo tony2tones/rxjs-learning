7a9448b76d35e725641bd16ce7c93109b295ae2c
Anthony Marques <anthony@armorica.gk>
2019-04-12 11:27:50 +0200

    adding new Rxjs stuff

---
+++ src/code.ts
@@ -0,0 +1,16 @@
+import { Observable } from "rxjs/Observable";
+
+var observable = Observable.create((observer: any) => {
+  observer.next("Hey dudies!");
+});
+
+observable.subscribe((x: any) => {
+  addItem(x);
+});
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById('output').appendChild(node);
+}
