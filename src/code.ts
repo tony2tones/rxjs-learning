7edcd1e39330b6535ad86b2c2271252b968385d0
Anthony Marques <anthony@armorica.gk>
2019-04-15 08:02:16 +0200

    updated src for observables

--- src/code.ts
+++ src/code.ts
@@ -1,20 +1,28 @@
 import { Observable } from "rxjs/Observable";
 
 var observable = Observable.create((observer: any) => {
-  observer.next("Hey dudies!");
-  observer.next("How are you?");
-  observer.next("I'm outski");
-  observer.complete();
-  observer.next("Hey dudies!");
+  try {
+    observer.next("Hey dudies!");
+    observer.next("How are you?");
+    setInterval(() => {
+        observer.next('Im good');
+    },2000);
+  } catch (err) {
+    observer.error(err);
+  }
 });
 
-observable.subscribe((x: any) => {
-  addItem(x);
+let observer = observable.subscribe((x: any) => {
+  addItem(x), (error: any) => addItem(error), () => addItem("Completed");
 });
 
-function addItem(val:any) {
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(val);
-    node.appendChild(textnode);
-    document.getElementById('output').appendChild(node);
+setTimeout(() =>{
+    observer.unsubscribe();
+}, 6001);
+
+function addItem(val: any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
 }
